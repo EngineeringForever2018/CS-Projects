4592b496bf1a343dd86bebaee5e379cdf6728d48
TheJaredLam <jared.h.lam@gmail.com>
2019-03-07 11:02:53 -0800

    Update Project CS 302

--- CS 302/Project 3/proj3.cpp
+++ CS 302/Project 3/proj3.cpp
@@ -13,35 +13,67 @@ using namespace std;
 int main() {
   int number[1000];
   fstream files;
-  //files.open("100000.txt");
-  //files.open("10000.txt");
-  files.open("1000.txt");
-  for(int i = 0; i < 1000; i++) {
+  //files.open("100000.txt");  //For file 100000.txt
+  //files.open("10000.txt");   //For file 10000.txt
+  files.open("1000.txt");      //For file 1000.txt
+  /* for(int i = 0; i < 100000; i++) {    //For file 100000.txt
+     //reading the files
+     files >> number[i];
+  } */
+  /* for(int i = 0; i < 10000; i++) {     //For file 10000.txt
+     //reading the files
+     files >> number[i];
+  } */
+  for(int i = 0; i < 1000; i++) {         //For file 1000.txt
      //reading the files
      files >> number[i];
   }
   files.close();
   clock_t start = clock();
-  quicksort(number, 0, 999);
+  //quicksort(number, 0, 99999); //For file 100000.txt
+  //quicksort(number, 0, 9999);  //For file 10000.txt
+  quicksort(number, 0, 999);     //For file 1000.txt
   clock_t end = clock();
-  for(int j = 0; j <= 999; j++) {
+  /* for(int j = 0; j <= 99999; j++) { //For file 100000.txt
+    cout << number[j] << endl;
+  } */
+  /* for(int j = 0; j <= 9999; j++) {  //For file 10000.txt
+    cout << number[j] << endl;
+  } */
+  for(int j = 0; j <= 999; j++) {      //For file 1000.txt
     cout << number[j] << endl;
   }
   cout << "CPU time: " << ((float)(end - start))/CLOCKS_PER_SEC << endl;
   cout << "Compare: " << COMPARE << endl;
   cout << "Swap: " << SWAP << endl;
-  //files.open("100000.txt");
-  //files.open("10000.txt");
-  files.open("1000.txt");
-  for(int i = 0; i < 1000; i++) {
+  //files.open("100000.txt");  //For file 100000.txt
+  //files.open("10000.txt");   //For file 10000.txt
+  files.open("1000.txt");      //For file 1000.txt
+  /* for(int i = 0; i < 100000; i++) {    //For file 100000.txt
+     //reading the files
+     files >> number[i];
+  } */
+  /* for(int i = 0; i < 10000; i++) {     //For file 10000.txt
+     //reading the files
+     files >> number[i];
+  } */
+  for(int i = 0; i < 1000; i++) {  //For file 1000.txt
      //reading the files
      files >> number[i];
   }
   files.close();
   start = clock();
+  //bubbleSort(number, 100000);  //wait for 30 second for the compile to read and sorted the size of the length
+  //bubbleSort(number, 10000);  //wait for 30 second for the compile to read and sorted the size of the length
   bubbleSort(number, 1000);  //wait for 30 second for the compile to read and sorted the size of the length
   end = clock();
-  for(int j = 0; j <= 999; j++) {
+  /* for(int j = 0; j <= 99999; j++) { //For file 100000.txt
+    cout << number[j] << endl;
+  } */
+  /* for(int j = 0; j <= 9999; j++) {  //For file 10000.txt
+    cout << number[j] << endl;
+  } */
+  for(int j = 0; j <= 999; j++) {      //For file 1000.txt
     cout << number[j] << endl;
   }
   cout << "CPU time: " << ((float)(end - start))/CLOCKS_PER_SEC << endl;
